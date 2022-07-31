78272d2f095359daccec073a6756352109e11ab8
learnGrowD <dev_willd@naver.com>
2022-07-31 22:21:19 +0900

    commit

---
+++ Ex12ST/Ex12ST/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ex12ST
+//
+//  Created by 도학태 on 2022/07/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
